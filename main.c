6ed02459ad6298c41c1d81ecb83d08f968261438
Santiago <slopez@fi.uba.ar>
2021-11-12 15:45:41 -0300

    Creo main de prueba de suma

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int suma(int a, int b);
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Se requieren 2 números para sumarlos\n\t$ ./suma <num1> <num2>\n");
+        return 1;
+    }
+
+    int a = atoi(argv[1]), b = atoi(argv[2]);
+    printf("%d\n", suma(a, b));
+
+    return 0;
+}
