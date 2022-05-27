3724b43c3157885683c8121d4e6b897764b54ce4
Santiago Lopez <slopez@fi.uba.ar>
2022-05-27 19:01:53 -0300

    Implemeto la funcion de multiplicacion de dos enteros

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int suma(int a, int b);
+int multiplicacion(int a, int b);
+
+int multiplicacion(int a, int b) {
+    return a * b;
+}
 
 int suma(int a, int b) {
     return a + b;
